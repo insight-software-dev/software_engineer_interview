e93f5d0c347d0492e7439ae5bfd65b7b5241f622
calebriley <cal3briley@gmail.com>
2024-09-01 21:17:12 +0100

    Base query setup.

--- challenges/gaps_in_tracks/app/app.py
+++ challenges/gaps_in_tracks/app/app.py
@@ -1,10 +1,36 @@
 from flask import Flask
+import mysql.connector
+import json
 
 app = Flask(__name__)
+
+config = {
+        'user': 'root',
+        'password': 'your_password',
+        'host': 'db',
+        'port': '3306',
+        'database': 'your_database',
+    }
+
+query = """SELECT elr_code, mileage_from, mileage_to FROM mileages"""
+
+def get_elrs(config, query):
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = [{"elr_code": elr_code, "mileage_from": mileage_from, "mileage_to": mileage_to,} for (elr_code, mileage_from, mileage_to) in cursor]
+    cursor.close()
+    connection.close()
+    return results
+
 
 @app.route('/')
 def index() -> str:
     return "running"
+
+@app.route('/elrs')
+def elrs() -> str:
+    return json.dumps(get_elrs(config=config, query=query))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
